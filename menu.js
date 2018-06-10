4173062234eeaa604a4384c620ac7165b78e71c5
anxenabaan <lemeneguillaume@hotmail.fr>
2018-06-10 21:39:43 +0200

    le menu il est beau

--- menu.js
+++ menu.js
@@ -3,12 +3,17 @@ var fermer = document.getElementById("btnclose")
 var ouvrir = document.getElementById("menuperso");
 
 profil.addEventListener("click", function(){
-  ouvrir.classList.toggle("hide");
+  ouvrir.classList.toggle("animate");
+  ouvrir.classList.remove("animatereverse");
+  //ouvrir.classList.toggle("hide");
+
   console.log("a");
 });
 
 
 fermer.addEventListener("click", function(){
-  ouvrir.classList.toggle("hide");
+  //ouvrir.classList.toggle("hide");
+  ouvrir.classList.toggle("animate");
+  ouvrir.classList.toggle("animatereverse");
   console.log("b");
 });
